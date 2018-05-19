390c3dac8533999e0d84d442479741cb155aedf1
Mikisho <mikiye.82@gmail.com>
2018-05-18 20:59:59 -0400

    workikng my read

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,20 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+
+import * as BooksAPI from '../utils/BooksAPI';
 
 import '../App.css';
+import BooksShelf from './BooksShelf';
 
-const SearchBooks = () => {
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <Link
-          className="close-search" to = "/"
-        >
-          Close
-        </Link>
-        <div className="search-books-input-wrapper">
-          {/*
+class SearchBooks extends Component {
+  state = {
+    query: '',
+    books: [],
+  };
+
+  handleUpdateQuery = query => {
+    BooksAPI.search (query).then (
+      books => (books ? this.setState ({books}) : [])
+    );
+    this.setState ({
+      query,
+    });
+  };
+
+  handleBookShelf (book, shelf) {
+    BooksAPI.update (book, shelf)
+      .then (
+        () =>
+          shelf !== 'none'
+            ? alert (`${book.title} succesfully added to shelf!`)
+            : null
+      )
+      .catch (() => alert ('error occured'));
+  }
+
+  displaySearcResults () {
+    const {books, query} = this.state;
+
+    if (query) {
+      return books.error
+        ? <div>search not found</div>
+        : books.map ((book, index) => {
+            return (
+              <BooksShelf
+                key={index}
+                book={book}
+                handleBookShelf={this.handleBookShelf.bind (this)}
+              />
+            );
+          });
+    }
+  }
+
+  render () {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            {/*
           NOTES: The search from BooksAPI is limited to a particular set of search terms.
           You can find these search terms here:
           https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -22,16 +67,29 @@ const SearchBooks = () => {
           However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
           you don't find a specific author or title. Every search is limited by search terms.
         */}
-          <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={event => this.handleUpdateQuery (event.target.value)}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  console.log (this.handleUpdateQuery ());
+                }
+              }}
+            />
 
+          </div>
         </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.displaySearcResults ()}
+          </ol>
+        </div>
+
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid" />
-      </div>
-    
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SearchBooks;
---
+++ src/components/shelfs.js
@@ -0,0 +1,12 @@
+const shelfs = {
+  none: 'none',
+  currently_Reading: 'Currently Reading',
+  want_To_Read: 'Want to Read',
+  already_read: 'Already Read',
+  //
+  currentlyReading: 'currentlyReading',
+  wantToRead: 'wantToRead',
+  read: 'read',
+};
+
+export default shelfs;
